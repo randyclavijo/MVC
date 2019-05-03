77107e34ee793c8b18da82f928b0c57f58ccbbc4
UserR <UserR@192.168.1.60>
2019-05-03 05:13:30 -0500

    CambioLocal

--- src/pruebarepositorio/PruebaRepositorio.java
+++ src/pruebarepositorio/PruebaRepositorio.java
@@ -16,6 +16,7 @@ public class PruebaRepositorio {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("hola mundo");
     }
     
 }
