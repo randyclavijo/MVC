d321a23111aa8b0bef0c96571b8f42585dca1e3d
UserR <UserR@192.168.1.60>
2019-05-04 08:51:38 -0500

    Opc4

--- src/pruebarepositorio/PruebaRepositorio.java
+++ src/pruebarepositorio/PruebaRepositorio.java
@@ -17,6 +17,7 @@ public class PruebaRepositorio {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hola mundo");
+        System.out.println("hola mundo");
     }
     
 }
